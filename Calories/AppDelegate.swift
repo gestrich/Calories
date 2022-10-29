33308d53894b71092a81b24d37af61daab1aff94
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2022-10-29 08:50:20 -0400

    Remove dead code related to local notifications that were causing deprecation warnings in Xcode

--- Calories/AppDelegate.swift
+++ Calories/AppDelegate.swift
@@ -16,10 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        let settings = UIUserNotificationSettings(types: UIUserNotificationType.badge, categories: nil)
-        UIApplication.shared.registerUserNotificationSettings(settings)
-        //UINavigationBar.appearance().setBackgroundImage(UIImage(), for: UIBarPosition.any, barMetrics: UIBarMetrics.default)
         
         UINavigationBar.appearance().tintColor = ThemeKit.baseColor()
         
