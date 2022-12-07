a3855b4aa2a2abdd0d44cccf527b521f6f7cfa51
Bill Gestrich <3207996+gestrich@users.noreply.github.com>
2022-12-07 05:56:26 -0600

    Add README

---
+++ Calories/IntestTest.swift
@@ -0,0 +1,9 @@
+//
+//  IntestTest.swift
+//  Calorie Log
+//
+//  Created by Bill Gestrich on 12/6/22.
+//  Copyright © 2022 Bill Gestrich. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,10 @@
+# Calorie Counter
+
+## App Store Deploy
+
+* Increment the build number
+* Increment the release number
+* Submit for review
+* Tag released commit.
+
+
